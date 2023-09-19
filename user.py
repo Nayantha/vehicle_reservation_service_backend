5a16bd6975bd2b4a17c49a795042227d3455d99c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-19 18:17:43 +0530

    create user class with class variables.

---
+++ user.py
@@ -0,0 +1,8 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class User(Model):
+    id = fields.IntField(pk=True)
+    username = fields.CharField(50, unique=True)
+    password_hash = fields.CharField(256)
