61d33ce37effe3c771f9fbf84c2a57a60c4f0b0c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-19 17:45:21 +0530

    add token generate method.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="")
+@app.post("/get_token")
+async def get_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    return {"access_token": form_data.username + "token"}
 
 @app.get("/")
 async def root():
