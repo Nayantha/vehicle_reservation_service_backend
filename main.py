3ccad7ea98a7e98fd5bec33bef5a70ae47a85322
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-19 18:47:13 +0530

    add db connect method.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from tortoise.contrib.fastapi import register_tortoise
+
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="")
@@ -15,3 +17,13 @@ async def root(token: str = Depends(oauth2_scheme)):
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+if __name__ == '__main__':
+    register_tortoise(
+        app,
+        db_url="",
+        modules={"models": ["user"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+
+    )
