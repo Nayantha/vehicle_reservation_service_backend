ff98277760061f6b20917a26790de2f574433f2f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-19 18:20:18 +0530

    add method to get user from username.

--- user.py
+++ user.py
@@ -6,3 +6,11 @@ class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(50, unique=True)
     password_hash = fields.CharField(256)
+
+    @classmethod
+    async def get_user(cls, username):
+        """
+        Get user fom username.
+        :param username: Username of the user        :return:
+        """
+        return cls.get(username=username)
